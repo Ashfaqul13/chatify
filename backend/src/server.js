a984eaedcc9c13273f1711261957c267c050818d
Ashfaqul13 <aasshfaq13@gmail.com>
2026-07-12 19:19:25 +0600

    fix: adjust path directory mapping

--- backend/src/server.js
+++ backend/src/server.js
@@ -17,12 +17,12 @@ app.use("/api/messages", messageRoutes);
 
 
 if(process.env.NODE_ENV === "production") {
-  // 1. Changed "../" to "../../" so it hops out of both 'src' and 'backend' folders
-  app.use(express.static(path.join(__dirname, "../../frontend/dist")))
+  // Change back to "../" so it points directly to /opt/render/project/frontend/dist
+  app.use(express.static(path.join(__dirname, "../frontend/dist")))
 
   app.get("*",(_,res) => {
-    // 2. Fixed the typo from "indexed.html" to "index.html"
-    res.sendFile(path.join(__dirname,"../../frontend", "dist", "index.html"))
+    // Change back to "../" here as well
+    res.sendFile(path.join(__dirname,"../frontend", "dist", "index.html"))
   });
 }
 
